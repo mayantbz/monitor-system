f9ca8a84ebb022b164e1b9c25f3b9de32a9a0f59
mayantbz <mayantbz@gmail.com>
2026-02-13 15:01:38 +0800

    locale.ts文件完成，引入persist持久化存储插件

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
